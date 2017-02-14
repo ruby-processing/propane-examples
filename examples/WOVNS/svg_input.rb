371e40787560640a79ad8b3063fa779f806a6faa
monkstone <mamba2928@yahoo.co.uk>
2017-02-14 14:30:34 +0000

    WOVNS examples

---
+++ examples/WOVNS/svg_input.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env jruby
+# frozen_string_literal: true
+require 'propane'
+# takes a bit of time to render
+# Divan Quality, Pastel 2 Palette
+class SVGInput < Propane::App
+
+  def settings
+    size(2400, 6372)
+    no_smooth
+  end
+
+  def setup
+    sketch_title 'SVG Input'
+    background(color('#696774')) # Reflector
+    # load an SVG from the data/ directory
+    s = load_shape(data_path('circles.svg'))
+    # draw 10 copies of the shape, with random sizes and locations
+    10.times do
+      scale = rand(0.5..2.0) # scale factor, from half to double the original size
+      # draw the shape at a random location, scaled according the random scale factor
+      shape(s, rand(width), rand(height), s.width * scale, s.height * scale)
+    end
+    save(data_path('random.png'))
+  end
+end
+
+SVGInput.new
---
+++ examples/WOVNS/svg_output.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env jruby
+# frozen_string_literal: true
+require 'propane'
+# Divan Spectrum 2
+class SVGOutput < Propane::App
+  load_library 'svg'
+  include_package 'processing.svg'
+
+  def settings
+    size(500, 500)
+    no_smooth
+  end
+
+  def setup
+    sketch_title 'SVG Output'
+    begin_record(SVG, data_path('output.svg'))
+    no_stroke
+    fill(color('#DA9AAD')) # Sedona
+    ellipse(width / 2, height / 2, width, height)
+    fill(color('#4688B5')) # Cyan
+    ellipse(width / 2, height / 2, width / 2, height / 2)
+    end_record
+    exit
+  end
+end
+
+SVGOutput.new
