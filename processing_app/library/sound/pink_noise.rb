fd1f412f0cafcf329712dd1e0e4a9bbd8b4d5f9c
monkstone <mamba2928@yahoo.co.uk>
2017-04-09 15:44:23 +0100

    sound samples

--- processing_app/library/sound/pink_noise.rb
+++ processing_app/library/sound/pink_noise.rb
@@ -1,17 +1,18 @@
-# This is a simple pink noise generator. It can be started with .play(amp).
-# In this example it is started and stopped by clicking into the renderer window.
+#!/usr/bin/env jruby
 require 'propane'
 
+# This is a simple pink noise generator. It can be started with .play(amp).
+# In this example it is started and stopped by clicking into the renderer window.
 class PinkNoiseApp < Propane::App
   load_library :sound
   include_package 'processing.sound'
-  
+
   attr_reader :amp, :noise
-  
+
   def settings
     size(640, 360)
   end
-  
+
   def setup
     sketch_title 'Pink Noise'
     background(255)
@@ -19,8 +20,8 @@ class PinkNoiseApp < Propane::App
     # Create the noise generator
     @noise = PinkNoise.new(self)
     noise.play(amp)
-  end      
-  
+  end
+
   def draw
     # Map mouseX from 0.0 to 1.0 for amplitude
     noise.amp(map1d(mouse_x, (0..width), (0.0..1.0)))
