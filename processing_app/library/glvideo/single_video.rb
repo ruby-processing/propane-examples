021c7a8b25f0d74eb8d8f644a0cbc5a7cfe038c1
monkstone <mamba2928@yahoo.co.uk>
2017-04-23 18:42:17 +0100

    glvideo

---
+++ processing_app/library/glvideo/single_video.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env jruby
+require 'propane'
+# The sketch class
+class SingleVideo < Propane::App
+
+  load_library :glvideo
+
+  include_package 'gohai.glvideo'
+
+  attr_reader :video
+
+  def setup
+    sketch_title 'Single Video'
+    @video = GLMovie.new(self, data_path('launch1.mp4'))
+    video.loop
+  end
+
+  def draw
+    background(0)
+    video.read if video.available
+    image(video, 0, 0, width, height)
+  end
+
+  def settings
+    size(560, 406, P2D)
+  end
+end
+
+SingleVideo.new
