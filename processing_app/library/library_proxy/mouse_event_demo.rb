65bc0edebc53fb7f81fb05c1bea01d6693425aad
monkstone <mamba2928@yahoo.co.uk>
2017-08-07 11:30:38 +0100

    mouse_event

---
+++ processing_app/library/library_proxy/mouse_event_demo.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env jruby
+require 'propane'
+
+class MouseEventDemo < Propane::App
+  load_libraries :library_proxy, :mouse_thing
+
+  def settings
+    size(800, 600)
+  end
+
+  def setup
+    sketch_title 'Mouse Event Demo'
+    MouseThing.new(self)
+  end
+
+  def draw
+    background(255)
+  end
+end
+
+MouseEventDemo.new
