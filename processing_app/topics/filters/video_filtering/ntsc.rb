ddea1971bea61be0360821e3c089863b43a9ef43
monkstone <mamba2928@yahoo.co.uk>
2017-08-15 07:12:35 +0100

    add filter sketches

---
+++ processing_app/topics/filters/video_filtering/ntsc.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env jruby
+require 'propane'
+# Hold mouse click to disable the filter temporarily
+# The videoclip is from NASA: http://youtu.be/CBwdZ1yloHA
+class Ntsc < Propane::App
+
+  load_library :video, :video_event
+  include_package 'processing.video'
+
+  attr_reader :movie, :my_shader
+
+  def settings
+    size(640, 480, P2D)
+  end
+
+  def setup
+    sketch_title 'Steinberg'
+    # Load and play the video in a loop
+    @movie = Movie.new(self, data_path('iss.mov'))
+    movie.loop
+    # Load and configure the shader
+    @my_shader = load_shader(data_path('shader.glsl'))
+    my_shader.set('sketchSize', width.to_f, height.to_f)
+  end
+
+  def draw
+    image(movie, 0, 0, width, height)
+    return if mouse_pressed?
+    filter(my_shader)
+  end
+
+  def movieEvent(m)
+    m.read
+  end
+end
+
+Ntsc.new
