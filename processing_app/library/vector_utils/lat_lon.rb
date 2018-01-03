6ab74c4337b29481f8440fcb0c506b84bbf45425
monkstone <mamba2928@yahoo.co.uk>
2018-01-03 11:38:50 +0000

    Uncouple CSV load from draw loop

--- processing_app/library/vector_utils/lat_lon.rb
+++ processing_app/library/vector_utils/lat_lon.rb
@@ -4,26 +4,34 @@ require 'propane'
 require 'arcball'
 require 'csv'
 # Capital name, Latitude and Longitude from csv
-class LatLong < Propane::App
 
+Place = Struct.new(:lat, :lon, :name)
+
+
+class LatLong < Propane::App
   load_library :vector_utils
   # Uses to_cartesian to map lat lon of cities, read from a csv file, on a globe
+  attr_reader :places
   def setup
     sketch_title 'Latitude and Longitude'
     noStroke
     textSize(9)
     Processing::ArcBall.init self
+    @places = []
+    CSV.foreach(data_path('capitals.csv'), headers: true) do |row|
+      lat = row['CapitalLatitude'].to_f
+      lon = row['CapitalLongitude'].to_f
+      name = row['CapitalName']
+      places << Place.new(lat, lon, name)
+    end
   end
 
   def draw
     background(20)
     lights
     counter = 0
-    CSV.foreach(data_path('capitals.csv'), headers: true) do |row|
-      lat = row['CapitalLatitude'].to_f
-      lon = row['CapitalLongitude'].to_f
-      name = row['CapitalName']
-      p = VectorUtil.to_cartesian(lat: lat, long: lon, radius: 300)
+    places.each do |place|
+      p = VectorUtil.to_cartesian(lat: place.lat, long: place.lon, radius: 300)
       push_matrix
       translate(p.x, p.y, p.z)
       polar = VectorUtil.cartesian_to_polar(vec: p)
@@ -31,9 +39,10 @@ class LatLong < Propane::App
       rotate_z(polar.z)
       push_matrix
       fill(255)
-      text(name,0,0) if (counter % 3).zero?
+      text(place.name,0,0) if (counter % 3).zero?
       pop_matrix
-      box(10, 3, 3)
+      fill(255, 255, 0, 100)
+      box(8, 3, 3)
       pop_matrix
       counter += 1
     end
