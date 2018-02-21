6f02a481ed52ef4d161cd8f173ae994bdbe2a96a
monkstone <mamba2928@yahoo.co.uk>
2018-02-21 11:08:39 +0000

    ImageProcessing examples

---
+++ external_library/java/image_processing/README.md
@@ -0,0 +1,5 @@
+The [image_processing library][image_processing] was created by [Nick 'Milchreis' Müller][Milchreis] and needs to be installed in your `~/.propane/libraries`, NB I use the convention of converting
+camelcase names to snakecase, I suggest you do the same for your propane libraries.
+
+[image_processing]:https://github.com/Milchreis/processing-imageprocessing
+[Milchreis]:https://milchreisjunkie.wordpress.com/
---
+++ external_library/java/image_processing/glitch_example.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/env jruby -v -W2
+require 'propane'
+
+# Example for an artificial glitch effect.
+# Move the mouse from left to right to see different intensity
+# or press the left mouse button to see the original image.
+#
+# Author: Nick 'Milchreis' Müller
+# Translated to JRubyArt by Martin Prout
+class GlitchExample < Propane::App
+  load_library :image_processing
+  java_import 'milchreis.imageprocessing.Glitch'
+
+  attr_reader :my_image
+
+  def setup
+    sketch_title 'Glitch'
+    # Load image
+    @my_image = load_image(data_path('example.jpg'))
+  end
+
+  def draw
+    return image(my_image, 0, 0) if mouse_pressed?
+    intensity = map1d(mouseX, 0..width, 0..4)
+    image(Glitch.apply(my_image, intensity), 0, 0)
+    # You can also use:
+    # Glitch.apply(image)
+    # Glitch.apply(image, intensity, scanlineHeight)
+  end
+
+  def settings
+    size(550, 550)
+  end
+end
+
+GlitchExample.new
