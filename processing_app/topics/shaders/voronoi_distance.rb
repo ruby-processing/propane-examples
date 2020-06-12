27998f7ea09d5f9ffb6db53eb987d8eb3a19830a
Martin Prout <mamba2928@yahoo.co.uk>
2020-06-12 15:35:41 +0100

    at last voronoi distance

---
+++ processing_app/topics/shaders/voronoi_distance.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/env jruby -v -W2
+# frozen_string_literal: true
+require 'propane'
+
+##
+# Voronoi Distance.
+#
+# GLSL version of the 1k intro Voronoi from the demoscene
+# (http://www.pouet.net/prod.php?which=52761)
+# Ported from the webGL version available in ShaderToy:
+# http://www.iquilezles.org/apps/shadertoy/
+# (Look for Voronoi under the Plane Deformations Presets)
+class VoronoiDistance < Propane::App
+  attr_reader :voronoi
+
+  def setup
+    sketch_title 'Voronoi Distance'
+    no_stroke
+    @voronoi = load_shader(data_path('voronoi_distance.glsl'))
+    voronoi.set('iResolution', width.to_f, height.to_f, 0.0)
+  end
+
+  def draw
+    voronoi.set('iTime', millis / 1000.0)
+    shader(voronoi)
+    # This kind of effects are entirely implemented in the
+    # fragment shader, they only need a quad covering the
+    # entire view area so every pixel is pushed through the
+    # shader.
+    rect(0, 0, width, height)
+  end
+
+  def settings
+    size(640, 360, P2D)
+  end
+end
+
+VoronoiDistance.new
